34bdaf5945b1376d9d6467dc9d9f6a681cb66c9d
Marcin_Zakidalski <marcin_Zakidalski@gmail.com>
2016-12-25 20:15:53 +0100

    Plot4 added

---
+++ plot4.R
@@ -0,0 +1,38 @@
+setwd("C:/Users/Marcin/Desktop/ExploratoryDataAnalysis/ExData_Plotting1")
+fileName <- "household_power_consumption.txt"
+Sys.setlocale("LC_ALL", "English")
+Sys.setenv("LANGUAGE"="En")
+
+data <- read.csv(fileName, header = TRUE, sep=";", stringsAsFactors = FALSE,
+                 na.strings = "?",
+                 colClasses = c("character","character","numeric","numeric","numeric","numeric",
+                                "numeric","numeric","numeric"))
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+data[,c("DateTime")] <- paste0(data[,c("Date")]," ",data[,c("Time")])
+data[, c("Date")] <- as.Date(data$Date, "%d/%m/%Y")
+data$Time <- strptime(x <- as.character(data$Time), format <- "%H:%M:%S")
+data$DateTime <- strptime(x <- as.character(data$DateTime), format <- "%d/%m/%Y %H:%M:%S")
+
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+
+#
+plot(data$DateTime,data$Global_active_power, typ = "l",
+     xlab="", ylab="Global Active Power")
+
+#
+plot(data$DateTime, data$Voltage,typ = "l",
+     xlab="datetime", ylab="Voltage")
+
+#
+plot(data$DateTime,data$Sub_metering_1, xlab="", ylab="Energy sub metering", typ = "l", col="black")
+lines(data$DateTime, data$Sub_metering_2,col="red")
+lines(data$DateTime,data$Sub_metering_3, col="blue")
+legend("topright",  lwd =1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty = "n")
+
+#
+plot(data$DateTime,data$Global_active_power, xlab="datetime", ylab="Global_Reactive_Power", typ = "l", col="black")
+
+dev.off()
+
