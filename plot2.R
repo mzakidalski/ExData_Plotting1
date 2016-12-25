0fa5dded46ab8f6f8d36a96649604b8d8009c0dd
Marcin_Zakidalski <marcin_Zakidalski@gmail.com>
2016-12-25 12:37:29 +0100

    plot2 finished

--- plot2.R
+++ plot2.R
@@ -1,5 +1,7 @@
 setwd("C:/Users/Marcin/Desktop/ExploratoryDataAnalysis/ExData_Plotting1")
 fileName <- "household_power_consumption.txt"
+Sys.setlocale("LC_ALL", "English")
+Sys.setenv("LANGUAGE"="En")
 
 data <- read.csv(fileName, header = TRUE, sep=";", stringsAsFactors = FALSE,
                          na.strings = "?",
@@ -13,7 +15,8 @@ data$Time <- strptime(x <- as.character(data$Time), format <- "%H:%M:%S")
 data$DateTime <- strptime(x <- as.character(data$DateTime), format <- "%d/%m/%Y %H:%M:%S")
 
 png("plot2.png", width = 480, height = 480)
-plot(data$DateTime,data$Global_active_power, typ = "l")
+plot(data$DateTime,data$Global_active_power, typ = "l",
+     xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
 
 
